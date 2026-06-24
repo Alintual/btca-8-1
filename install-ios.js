f90e3f9a6394fbb013510b2aea239a55bcde2b6a
unknown <C:\Users\User\AppData\Roaming\The Bat!>
2026-06-24 19:50:30 +0700

    Deploy btca-web-8.1.79 (verified build)

--- install-ios.js
+++ install-ios.js
@@ -2,8 +2,8 @@
   "use strict";
 
   var BTCA_BASE = "/btca-8-1/";
-  var INSTALL_CACHE = "btca-web-8.1.78:static-install";
-  var MEDIA_CACHE = "btca-web-8.1.78:static-media";
+  var INSTALL_CACHE = "btca-web-8.1.79:static-install";
+  var MEDIA_CACHE = "btca-web-8.1.79:static-media";
   var MEDIA_PROBE_RE = /offline-unpacked\/level1\/exercises\/[^/]+\.(jpe?g|png|webp|gif)$/i;
   var MEDIA_STATE_KEY = "btca-web:static-media-state";
   var APP_READY_KEY = "btca-web:app-ready";
@@ -302,21 +302,12 @@
     resetHomePhraseInlineLayout();
   }
 
-  function applyInstalledPhraseOneNudge() {
-    var el = document.querySelector(".home__phrase--one");
-    if (!el) return;
-    var rootFont = parseFloat(getComputedStyle(document.documentElement).fontSize) || 17;
-    var shift = (10 * rootFont) / 17;
-    el.style.transform = "translate(" + shift + "px, " + shift + "px) rotate(45deg)";
-  }
-
   function syncHomeTaglineLayout() {
     if (!document.body.classList.contains("btca-installed-mode")) {
       resetLoadingHomePhraseLayout();
       return;
     }
     resetHomePhraseInlineLayout();
-    applyInstalledPhraseOneNudge();
   }
 
   function clearComfortTypography() {
