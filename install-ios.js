4b6cfcbb2b22892f936fe9218bb1b88a3ab0600a
unknown <C:\Users\User\AppData\Roaming\The Bat!>
2026-06-26 08:46:01 +0700

    Deploy btca-web-8.1.108 (verified build)

--- install-ios.js
+++ install-ios.js
@@ -2,8 +2,8 @@
   "use strict";
 
   var BTCA_BASE = "/btca-8-1/";
-  var INSTALL_CACHE = "btca-web-8.1.107:static-install";
-  var MEDIA_CACHE = "btca-web-8.1.107:static-media";
+  var INSTALL_CACHE = "btca-web-8.1.108:static-install";
+  var MEDIA_CACHE = "btca-web-8.1.108:static-media";
   var MEDIA_PROBE_RE = /offline-unpacked\/level1\/exercises\/[^/]+\.(jpe?g|png|webp|gif)$/i;
   var MEDIA_STATE_KEY = "btca-web:static-media-state";
   var APP_READY_KEY = "btca-web:app-ready";
