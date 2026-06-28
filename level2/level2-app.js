daaf5cf7473097691368618c69aaf0fcb56b39aa
unknown <C:\Users\User\AppData\Roaming\The Bat!>
2026-06-28 09:51:59 +0700

    Deploy btca-web-8.1.138 (verified build)

--- level2/level2-app.js
+++ level2/level2-app.js
@@ -3,7 +3,7 @@
 
   var DB = window.BTCA_LEVEL2_DB;
   var BAZA = window.BTCA_LEVEL2_BAZA;
-  var VERSION = "8.1.67";
+  var VERSION = "8.1.68";
   var BRANDING_UP = "branding/up.png";
   var BRANDING_BAZA = "branding/baza.png";
   var TRAILING_SLOT_W = 112;
