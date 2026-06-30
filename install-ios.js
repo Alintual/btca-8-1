409dc20e0db55a697a9a575eebf4e41d582b196b
unknown <C:\Users\User\AppData\Roaming\The Bat!>
2026-06-30 11:12:53 +0700

    Deploy btca-web-8.1.166 (verified build)

--- install-ios.js
+++ install-ios.js
@@ -2,8 +2,8 @@
   "use strict";
 
   var BTCA_BASE = "/btca-8-1/";
-  var INSTALL_CACHE = "btca-web-8.1.165:static-install";
-  var MEDIA_CACHE = "btca-web-8.1.165:static-media";
+  var INSTALL_CACHE = "btca-web-8.1.166:static-install";
+  var MEDIA_CACHE = "btca-web-8.1.166:static-media";
   var MEDIA_PROBE_RE = /offline-unpacked\/level1\/exercises\/[^/]+\.(jpe?g|png|webp|gif)$/i;
   var MEDIA_STATE_KEY = "btca-web:static-media-state";
   var APP_READY_KEY = "btca-web:app-ready";
@@ -312,11 +312,8 @@
     return "БТКА 8.1";
   }
 
-  function hasInstalledAppMarkers() {
-    if (readInstallSession()) return true;
-    if (hasHomeShortcutMarker()) return true;
-    if (readAppPreparedState() && hasPreparedMediaState()) return true;
-    return false;
+  function hasActiveShortcutMarkers() {
+    return Boolean(readInstallSession() || hasHomeShortcutMarker());
   }
 
   function hasHomeShortcutMarker() {
@@ -380,12 +377,36 @@
     });
   }
 
+  function reconcileIosShortcutMarkers() {
+    if (isStandalone() || (!isAppleMobile() && !isDebugAppleMode())) {
+      return Promise.resolve();
+    }
+    return probeInstalledRelatedApps().then(function (apiResult) {
+      if (apiResult === true) {
+        markHomeShortcutPresent();
+        return;
+      }
+      if (apiResult === false) {
+        clearShortcutPresenceMarkers();
+        return;
+      }
+      if (!readInstallSession() && hasHomeShortcutMarker()) {
+        try {
+          localStorage.removeItem(HOME_SHORTCUT_KEY);
+        } catch (_) {}
+      }
+    });
+  }
+
   function detectHomeScreenShortcut() {
     if (isStandalone()) return Promise.resolve(false);
     return probeInstalledRelatedApps().then(function (apiResult) {
       if (apiResult === true) return true;
-      if (apiResult === false) return false;
-      return hasInstalledAppMarkers();
+      if (apiResult === false) {
+        clearShortcutPresenceMarkers();
+        return false;
+      }
+      return hasActiveShortcutMarkers();
     });
   }
 
@@ -1666,12 +1687,17 @@
       return;
     }
 
-    detectHomeScreenShortcut().then(function (hasShortcut) {
+    reconcileIosShortcutMarkers()
+      .then(function () {
+        return detectHomeScreenShortcut();
+      })
+      .then(function (hasShortcut) {
       if (hasShortcut) {
         renderShortcutRemovalWarning(resolvePwaShortcutName());
         setButtonState(false, "Загрузить все данные для offline");
         return;
       }
+      clearShortcutPresenceMarkers();
       beginOfflinePreparation();
     });
   }
@@ -1770,7 +1796,7 @@
           els.button.addEventListener("click", prepareOffline);
         }
         if (!isStandalone()) {
-          syncInstallSessionWithShortcutPresence();
+          syncInstallSessionWithShortcutPresence().then(reconcileIosShortcutMarkers);
         }
         if (isStandalone()) {
           return wipeTrainingDatabasesOnReinstall().then(function () {
@@ -1788,7 +1814,7 @@
           els.button.addEventListener("click", prepareOffline);
         }
         if (!isStandalone()) {
-          syncInstallSessionWithShortcutPresence();
+          syncInstallSessionWithShortcutPresence().then(reconcileIosShortcutMarkers);
         }
         if (isStandalone()) {
           return wipeTrainingDatabasesOnReinstall().then(function () {
