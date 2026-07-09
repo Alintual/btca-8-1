ec9c553c0cf25ffa1d7318f6dca26e9204b8c1d4
unknown <C:\Users\User\AppData\Roaming\The Bat!>
2026-07-09 18:43:25 +0700

    Deploy btca-web-8.1.262 (verified build)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "btca-web-8.1.261";
+const CACHE_VERSION = "btca-web-8.1.262";
 const APP_CACHE = `${CACHE_VERSION}:app`;
 const RUNTIME_CACHE = `${CACHE_VERSION}:runtime`;
 const BASE_PATH = "/btca-8-1";
